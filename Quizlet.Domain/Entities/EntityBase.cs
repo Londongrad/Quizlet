4b783ef648c49570cff8714e9ffc48e55aedac30
Londongrad <teslagradus@gmail.com>
2025-06-26 17:48:40 +0900

    Initial clean setup

--- Quizlet.Domain/Entities/EntityBase.cs
+++ Quizlet.Domain/Entities/EntityBase.cs
@@ -3,6 +3,6 @@
     public class EntityBase(Guid id)
     {
         public Guid Id { get; private set; } = id;
-        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+        public DateTime CreatedAt { get; private set; } = DateTime.Now; 
     }
 }
